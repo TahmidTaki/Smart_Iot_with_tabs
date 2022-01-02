5349fbeff17fd0593e5ac5944442e953e8a8958f
TahmidTaki <tahmid.taki@live.iium.edu.my>
2022-01-02 18:14:28 +0800

    final commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+/* import 'package:flutter_smart_iot_home/landing_screen.dart'; */
+import 'package:api_example_app/landing_screen.dart';
+import 'package:api_example_app/HomeScreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Smart Home',
+      theme: ThemeData(
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          fontFamily: 'Poppins'),
+      home: Home_Screen(),
+    );
+  }
+}
